0203f47ff6742e45344d62224f72bb7bc0cdc862
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-05-06 15:24:19 -0500

    Add portfolio image

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects() {
     {
       name: "Pokedex Compendium",
       description:
-        "Single page app highlighting information regarding Pokemons, berries, and items in the game series built using React.",
+        "Single page app highlighting information regarding Pokemons, berries, and items in the game series built using React.js.",
       image: "./Pokedex.png",
       code: "https://github.com/Fiestaman/pokedex",
       demo: "https://pokedexcompendium.netlify.app/",
@@ -22,16 +22,16 @@ export default function Projects() {
     {
       name: "annson.dev",
       description: "Portfolio website built using React.js.",
-      image: "",
+      image: "./Portfolio.png",
       code: "https://github.com/Fiestaman/fiestaman.github.io",
-      demo: "annson.dev",
+      demo: "https://annson.dev/",
     },
     // { name: "", description: "", image: "", code: "", demo: "" },
   ];
 
   const [projects, setProjects] = useState([]);
 
-  useEffect(() => setProjects(arr), []);
+  useEffect(() => setProjects(arr), [arr]);
 
   const cards = projects.map((card, i) => {
     return <ProjectsCard key={`Card${i}`} card={card} />;
